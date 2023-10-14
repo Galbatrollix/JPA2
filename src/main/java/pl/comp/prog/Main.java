d8d12fe790fe09c7f441b07e01d29259af0c9d40
Galbatrollix <78318978+Galbatrollix@users.noreply.github.com>
2023-10-14 21:22:21 +0200

    initial commit

---
+++ src/main/java/pl/comp/prog/Main.java
@@ -0,0 +1,19 @@
+package pl.comp.prog;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import model.Student;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("test");
+        EntityManager em = emf.createEntityManager();
+        Student student = new Student();
+        student.setName("Kowalski");
+        em.getTransaction().begin();
+        em.persist(student);
+        em.getTransaction().commit();
+    }
+}
